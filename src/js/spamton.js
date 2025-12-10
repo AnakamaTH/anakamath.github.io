65d2cc9fc563813ae7b9c6aa0fa7be2afb01f0cc
Anakama <109427414+AnakamaTH@users.noreply.github.com>
2025-12-10 21:48:55 +0300

    Create spamton.js

---
+++ src/js/spamton.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const boxes = document.querySelectorAll(".box");
+  const voice = new Audio("assets/spamton_pitch.wav");
+  voice.volume = 0.4;
+
+  async function typeBox(box) {
+    const text = box.textContent.trim();
+    const words = text.split(" ");
+    
+    box.textContent = "";
+    for (let i = 0; i < words.length; i++) {
+      box.textContent += words[i] + " ";
+      
+      voice.currentTime = 0;
+      voice.play();
+
+      await new Promise(res => setTimeout(res, 110));
+    }
+  }
+
+  async function playAll() {
+    for (let box of boxes) {
+      await typeBox(box);
+      await new Promise(res => setTimeout(res, 400));
+    }
+  }
+
+  playAll();
+});
