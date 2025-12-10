855622c709d85bbf356f08743588db2341e3482f
Anakama <109427414+AnakamaTH@users.noreply.github.com>
2025-12-10 22:07:48 +0300

    Delete src/js/spamton_into.js

--- src/js/spamton_into.js
+++
@@ -1,9 +0,0 @@
-const bgm = new Audio("assets/spamton.ogg");
-bgm.loop = true;
-bgm.volume = 0.5;
-
-bgm.play().catch(() => {
-  document.addEventListener("click", () => {
-    bgm.play();
-  }, { once: true });
-});
